6a1286be1fdff9b00854567fef4c7e0c8d0ee67f
lavor <lavor.andrey@gmail.com>
2024-07-24 15:27:03 +0600

    Update

--- src/test/java/tech/mindstructs/structures/properties/IxoraPropertiesToDataMapperTest.java
+++ src/test/java/tech/mindstructs/structures/properties/IxoraPropertiesToDataMapperTest.java
@@ -1,12 +1,12 @@
 package tech.mindstructs.structures.properties;
 
 import intellispaces.ixora.mindstructs.structures.properties.PropertiesToDataMapper;
-import tech.mindstructs.test.structures.properties.PropertiesToDataGuideTest;
+import tech.mindstructs.test.structures.properties.PropertiesToDataMapperTest;
 
 /**
  * Tests for {@link IxoraPropertiesToDataMapper} class.
  */
-public class IxoraPropertiesToDataMapperTest extends PropertiesToDataGuideTest {
+public class IxoraPropertiesToDataMapperTest extends PropertiesToDataMapperTest {
 
   @Override
   public PropertiesToDataMapper guide() {
