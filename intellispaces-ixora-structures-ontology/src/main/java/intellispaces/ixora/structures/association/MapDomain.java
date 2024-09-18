67be8d11fcf63f01e56c66cde9d15665db0cc5b1
lavor <lavor.andrey@gmail.com>
2024-09-18 15:09:47 +0600

    Remove isAbstract flag of domain

--- intellispaces-ixora-structures-ontology/src/main/java/intellispaces/ixora/structures/association/MapDomain.java
+++ intellispaces-ixora-structures-ontology/src/main/java/intellispaces/ixora/structures/association/MapDomain.java
@@ -4,7 +4,7 @@ import intellispaces.framework.core.annotation.Domain;
 import intellispaces.framework.core.annotation.Transition;
 import intellispaces.ixora.structures.collection.CollectionDomain;
 
-@Domain(value = "7fe5d4bb-a298-45a2-acc8-75b6bb4cb2f0", isAbstract = true)
+@Domain(value = "7fe5d4bb-a298-45a2-acc8-75b6bb4cb2f0")
 public interface MapDomain<K, V> {
 
   @Transition("b766a35c-b544-4b84-9b5e-51dc8386e3cd")
