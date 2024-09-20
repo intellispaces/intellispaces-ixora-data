6c9931490e4a2b92be7571ff44136040b6e5591b
lavor <lavor.andrey@gmail.com>
2024-09-20 15:02:59 +0600

    New methods to Maps class added

--- intellispaces-ixora-structures/src/main/java/intellispaces/ixora/structures/association/Maps.java
+++ intellispaces-ixora-structures/src/main/java/intellispaces/ixora/structures/association/Maps.java
@@ -13,6 +13,30 @@ public interface Maps {
   }
 
   static <K, V> UnmovableMap<K, V> of(
+    Class<K> keyClass, Class<V> valueClass,
+    K key, V value
+  ) {
+    return new JavaMapHandleImpl<>(java.util.Map.of(key, value), keyClass, valueClass);
+  }
+
+  static <K, V> UnmovableMap<K, V> of(
+    Class<K> keyClass, Class<V> valueClass,
+    K key1, V value1,
+    K key2, V value2
+  ) {
+    return new JavaMapHandleImpl<>(java.util.Map.of(key1, value1, key2, value2), keyClass, valueClass);
+  }
+
+  static <K, V> UnmovableMap<K, V> of(
+    Class<K> keyClass, Class<V> valueClass,
+    K key1, V value1,
+    K key2, V value2,
+    K key3, V value3
+  ) {
+    return new JavaMapHandleImpl<>(java.util.Map.of(key1, value1, key2, value2, key3, value3), keyClass, valueClass);
+  }
+
+  static <K, V> UnmovableMap<K, V> of(
     Type<K> keyType, Type<V> valueType,
     K key, V value
   ) {
