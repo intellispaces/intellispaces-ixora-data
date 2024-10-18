d93e30e8d729aee4931d3932e4acb04cf87427bb
lavor <lavor.andrey@gmail.com>
2024-10-18 11:02:29 +0600

    Channels readMultiple and readAll added to InputDataStreamDomain

--- intellispaces-ixora-data-ontology/src/main/java/intellispaces/ixora/data/datastream/ByteInputStreamDomain.java
+++ intellispaces-ixora-data-ontology/src/main/java/intellispaces/ixora/data/datastream/ByteInputStreamDomain.java
@@ -2,15 +2,33 @@ package intellispaces.ixora.data.datastream;
 
 import intellispaces.framework.core.annotation.Channel;
 import intellispaces.framework.core.annotation.Domain;
+import intellispaces.framework.core.annotation.Unmovable;
 import intellispaces.framework.core.traverse.TraverseTypes;
+import intellispaces.ixora.data.collection.ByteListDomain;
 
 @Domain("e414189a-f906-49bd-9a89-116c7c19debd")
 public interface ByteInputStreamDomain extends InputDataStreamDomain<Byte> {
 
-  @Override
   @Channel(
       value = "47a07883-5660-4e7e-81f1-24d48d215285",
-      name = "InputByteStreamToNextElementChannel",
+      name = "ByteInputStreamReadChannel",
       allowedTraverse = TraverseTypes.MappingOfMoving)
+  @Override
   Byte read();
+
+  @Channel(
+      value = "2c9b4074-f83b-4d4f-983d-3107e26dc8ee",
+      name = "ByteInputStreamReadMultipleChannel",
+      allowedTraverse = TraverseTypes.MappingOfMoving)
+  @Unmovable
+  @Override
+  ByteListDomain readMultiple(int number);
+
+  @Channel(
+      value = "0ffb808a-2749-43f8-9afe-b43337faf280",
+      name = "ByteInputStreamReadAllChannel",
+      allowedTraverse = TraverseTypes.MappingOfMoving)
+  @Unmovable
+  @Override
+  ByteListDomain readAll();
 }
--- intellispaces-ixora-data-ontology/src/main/java/intellispaces/ixora/data/datastream/InputDataStreamDomain.java
+++ intellispaces-ixora-data-ontology/src/main/java/intellispaces/ixora/data/datastream/InputDataStreamDomain.java
@@ -2,7 +2,9 @@ package intellispaces.ixora.data.datastream;
 
 import intellispaces.framework.core.annotation.Channel;
 import intellispaces.framework.core.annotation.Domain;
+import intellispaces.framework.core.annotation.Unmovable;
 import intellispaces.framework.core.traverse.TraverseTypes;
+import intellispaces.ixora.data.collection.ListDomain;
 
 @Domain("671f5295-423a-4b26-a579-6c3087c02e88")
 public interface InputDataStreamDomain<E> extends DataStreamDomain<E> {
@@ -14,7 +16,21 @@ public interface InputDataStreamDomain<E> extends DataStreamDomain<E> {
 
   @Channel(
       value = "e9dc6abd-5f7c-42f8-8e4b-d0ceb90adf06",
-      name = "InputDataStreamReadElementChannel",
+      name = "InputDataStreamReadChannel",
       allowedTraverse = TraverseTypes.MappingOfMoving)
   E read();
+
+  @Channel(
+      value = "f7415895-f183-43e0-8fc6-c50fe08e3c98",
+      name = "InputDataStreamReadMultipleChannel",
+      allowedTraverse = TraverseTypes.MappingOfMoving)
+  @Unmovable
+  ListDomain<E> readMultiple(int number);
+
+  @Channel(
+      value = "56d9023d-2045-4b8b-96d7-f2beb64cb378",
+      name = "InputDataStreamReadAllChannel",
+      allowedTraverse = TraverseTypes.MappingOfMoving)
+  @Unmovable
+  ListDomain<E> readAll();
 }
